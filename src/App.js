c37ad30c4c452bc44eea5f109d08df78a48dea34
Pedro Nest <pedronestjp@gmail.com>
2023-07-11 06:35:50 -0300

    up header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import {Header} from "../src/components/Header"
+import AboutUs from './components/AboutUs';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <AboutUs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "../styles/Header.css";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container">
+          <div className="logo">
+            {" "}
+            <img className="logoImg" src="./img/logo.png" alt="" />{" "}
+          </div>
+        <div className="navbar">
+          <div className="containerBox">
+            <div className="lists">
+              <div className="list">
+                <span className="listLinks"> Inicio </span>
+                <span className="listLinks"> Sobre </span>
+                <span className="listLinks"> Serviços </span>
+                <span className="listLinks"> Aentendimentos </span>
+              </div>
+              <div className="list2">
+                <span className="listLinks2">
+                  {" "}
+                  <img src="./img/instagram.svg" alt="linkIstagram" />{" "}
+                </span>
+                <span className="listLinks2">
+                  {" "}
+                  <img src="./img/whatsapp.svg" alt="LinkWhatsapp" />{" "}
+                </span>
+                <span> (83) 9 86377109 </span>
+              </div>
+            </div>
+            <div className="slogan">
+              <div className="sloganHeader">
+                <span className="text-wrapper">A</span>
+                <span className="span">&nbsp;</span>
+                <span className="text-wrapper-2">
+                  IMPORTÂNCIA
+                  <br />
+                </span>
+                <span className="text-wrapper">DO</span>
+                <span className="text-wrapper-2">
+                  {" "}
+                  SEU <br />
+                  NEGÓCIO DIGITAL
+                  <br />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="services">
+          <div className="cards">
+            <div className="card" id="cardSeo">
+              <img className="cardImg" src="./img/seovalue-1.png" alt="seo" />
+              <p className="cardTitle"> Oimização de busca </p>
+            </div>
+            <div className="card" id="cardWebDev">
+              <img
+                className="cardImg"
+                src="./img/wavy-bus-27-single-01-photoroom-1.png"
+                alt=""
+              />
+              <p className="cardTitle"> Criação de sites e apps </p>
+            </div>
+            <div className="card" id="cardEmMkt">
+              <img className="cardImg" src="./img/email-1.png" alt="" />
+              <p className="cardTitle"> Campanha de email marketing</p>
+            </div>
+            <div className="card" id="cardSocialNet">
+              <img
+                className="cardImg"
+                src="./img/6544292-photoroom-1.png"
+                alt=""
+              />
+              <p className="cardTitle"> Gerenciamento de redes sociais</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
